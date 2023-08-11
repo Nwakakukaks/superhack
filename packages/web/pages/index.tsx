3dca93fb90f96c1cab1fa75ced946676ed95b6d2
Apple MacBook Pro <user@Azubuikes-MacBook-Pro.local>
2023-08-11 14:06:44 +0100

    asset images

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -63,7 +63,7 @@ const MainLanding: NextPage = () => {
               <Col lg={7} md={12} sm={12}>
                 <div className="feature-image">
                   <img
-                    src="/assets/images/screenshot.png"
+                    src="/assets/images/homeblock.png"
                     alt="screenshot"
                     className="framed-image"
                   />
