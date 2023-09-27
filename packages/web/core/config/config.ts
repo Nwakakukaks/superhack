eefbbaaefc8a0fcbab99cf9275eb559e8b4bc983
nwakakukaks <winnernwakaku123@gmail.com>
2023-09-27 03:20:31 +0100

    pego config.

--- packages/web/core/config/config.ts
+++ packages/web/core/config/config.ts
@@ -1,17 +1,14 @@
 export interface AppConfig {
   environment: string;
   jwtSecret: string;
-  optimismUrl: string;
-  baseUrl: string;
-  zoraUrl: string;
-  modeUrl: string;
+  pegoMainnetUrl: string;
+  pegoTestnetUrl: string;
+ 
 }
 
 export const Config: AppConfig = {
   environment: process.env.NEXT_PUBLIC_ENVIRONMENT || 'local',
   jwtSecret: process.env.JWT_SECRET || 'jwtsecret',
-  optimismUrl: '',
-  baseUrl: '',
-  zoraUrl: '',
-  modeUrl: '',
+  pegoMainnetUrl: '',
+  pegoTestnetUrl: '',
 };
