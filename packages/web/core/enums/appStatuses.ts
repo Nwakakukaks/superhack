d285ca3e801e514c402ab5f2eb544c3fab17cc6b
nwakakukaks <winnernwakaku123@gmail.com>
2023-09-27 03:24:50 +0100

    status.

--- packages/web/core/enums/appStatuses.ts
+++ packages/web/core/enums/appStatuses.ts
@@ -1,8 +1,6 @@
 export enum AppStatuses {
   PendingContract = 'PendingContract',
   Compiled = 'Compiled',
-  DeployedToOptimism = 'DeployedToOptimism',
-  DeployedToBase = 'DeployedToBase',
-  DeployedToZora = 'DeployedToZora',
-  DeployedToMode = 'DeployedToMode',
+  DeployedToPegoMainnet = 'DeployedToPegoMainnet',
+  DeployedToPegoTestnet = 'DeployedToPegoTestnet',
 }
